ec9f0a691a6276329afe7991c9f32ac7c7196f92
be <beatrizg.m@yahoo.com>
2023-02-14 11:26:58 -0300

    Tooltip creation and configuration

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initAnimaScroll from './modules/scroll-animacao.js';
 import initAccordion from './modules/accordion.js';
 import initTab from './modules/tabnav.js';
 import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
 
 initScrollSuave();
 initAnimaScroll();
 initAccordion();
 initTab();
 initModal();
+initTooltip();
