f68fa4903f2e06793ef52f63506026176744306e
be <beatrizg.m@yahoo.com>
2023-02-16 15:37:05 -0300

    add menu-mobile

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ import initTab from './modules/tabnav.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDropDownMenu from './modules/dropdown-menu.js';
+import initMenuModible from './modules/menu-mobile.js';
 
 initScrollSuave();
 initAnimaScroll();
@@ -13,3 +14,4 @@ initTab();
 initModal();
 initTooltip();
 initDropDownMenu();
+initMenuModible();
